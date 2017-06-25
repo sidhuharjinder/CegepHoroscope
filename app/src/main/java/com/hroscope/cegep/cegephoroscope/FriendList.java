3731ad984539feff9ccce7a9fc02773be77fbe81
Parth <parth1493@gmail.com>
2017-06-25 10:50:15 -0400

    Navigation code on profile, signup,homescreen by Parth

--- app/src/main/java/com/hroscope/cegep/cegephoroscope/FriendList.java
+++ app/src/main/java/com/hroscope/cegep/cegephoroscope/FriendList.java
@@ -8,6 +8,6 @@ public class FriendList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend_list);
+        //setContentView(R.layout.activity_friend_list);
     }
 }
--- app/src/main/java/com/hroscope/cegep/cegephoroscope/SignupFragment.java
+++ app/src/main/java/com/hroscope/cegep/cegephoroscope/SignupFragment.java
@@ -3,9 +3,11 @@ package com.hroscope.cegep.cegephoroscope;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -26,7 +28,17 @@ public class SignupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        Button facebookButton = (Button)view.findViewById(R.id.Facebook);
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout,Profile.newInstance());
+                transaction.commit();
+            }
+        });
+        return view;
     }
 
 }
