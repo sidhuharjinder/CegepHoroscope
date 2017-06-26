9bbecdd59fe28c571b77be77a5a6de31eeae3e0b
Parth <parth1493@gmail.com>
2017-06-26 12:12:12 -0400

    Friend list and friend profile navigation

--- app/src/main/java/com/hroscope/cegep/cegephoroscope/FriendList.java
+++ app/src/main/java/com/hroscope/cegep/cegephoroscope/FriendList.java
@@ -1,13 +1,24 @@
 package com.hroscope.cegep.cegephoroscope;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FriendList extends AppCompatActivity {
-
+ImageView addFriendButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+        addFriendButton = (ImageView)findViewById(R.id.addFriendButton);
+        addFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FriendList.this,EditandDeleteFriend.class);
+                startActivity(intent);
+            }
+        });
     }
 }
