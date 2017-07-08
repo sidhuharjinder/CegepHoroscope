7966ed7274c695749318b35381675ceb6c6d7f80
SACHIN <smart.san70@gmail.com>
2017-07-08 15:18:44 -0400

    Saving Friend_Info Done

--- app/src/main/java/com/hroscope/cegep/cegephoroscope/Friends_List/Friend_Edit_DeleteFragment.java
+++ app/src/main/java/com/hroscope/cegep/cegephoroscope/Friends_List/Friend_Edit_DeleteFragment.java
@@ -9,10 +9,13 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -23,15 +26,18 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.hroscope.cegep.cegephoroscope.R;
-import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -87,7 +93,7 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
 
         view =  inflater.inflate(R.layout.activity_editand_delete_friend, container, false);
          setControlls();
-        if (firebaseAuth.getCurrentUser() != null) {
+       /* if (firebaseAuth.getCurrentUser() != null) {
             //check image file is exist on location or not
             storageRef.child("Email_Registration").child("Users_Friend_Images").child(userUID).child("image.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                 @Override
@@ -102,9 +108,10 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
                 }
             });
 
-        }
+        }*/
        // ReadFirebase_setProfileData();
-        //setDataTime();
+        setDataTime();
+        pd.dismiss();
         return view;
     }
     public void setControlls()
@@ -167,6 +174,7 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
         pd.setMessage("Uploading....");
     }
 
+
     public void fromGallary() {
         Intent intent = new Intent();
         intent.setType("image/*");
@@ -175,7 +183,7 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
         startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
     }
 
-    public void upload()
+    public void upload(String UniqueID)
     {
         if(filePath != null) {
             pd.show();
@@ -183,7 +191,7 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
             String useruid=firebaseAuth.getCurrentUser().getUid();
 
 
-            StorageReference databaseReference = storageRef.child("Email_Registration").child("Users_Friend_Images").child(useruid).child("image.jpg");
+            StorageReference databaseReference = storageRef.child("Email_Registration").child("Users_Friend_Images").child(useruid).child(UniqueID).child("image.jpg");
 
             //uploading the image
             UploadTask uploadTask = databaseReference.putFile(filePath);
@@ -230,7 +238,109 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
 
         button_gallary.setVisibility(view.VISIBLE);
 
+    }
 
+    public void updateData_loadTofirebase(String UniqueID)
+    {
+
+        pd.setMessage("Saving....");
+        birthdate=dateOfBirth.getText().toString();
+        updated_email=name.getText().toString();
+
+        int day=Integer.parseInt(birthdate.substring(0,2));
+        int month= Integer.parseInt(birthdate.substring(3,5));
+        int year=Integer.parseInt(birthdate.substring(6,10));
+
+        //Get date wise zodiac Sign
+        if ((month == 12 && day >= 22 && day <= 31) || (month ==  1 && day >= 01 && day <= 19))
+            zodiac_sign_name="Capricorn";
+
+        else if ((month ==  01 && day >= 20 && day <= 31) || (month ==  2 && day >= 01 && day <= 17))
+            zodiac_sign_name="Aquarius";
+        else if ((month ==  02 && day >= 18 && day <= 29) || (month ==  3 && day >= 01 && day <= 19))
+            zodiac_sign_name="Pisces";
+        else if ((month ==  03 && day >= 20 && day <= 31) || (month ==  4 && day >= 01 && day <= 19))
+            zodiac_sign_name="Aries";
+        else if ((month ==  04 && day >= 20 && day <= 30) || (month ==  5 && day >= 01 && day <= 20))
+            zodiac_sign_name="Taurus";
+        else if ((month ==  05 && day >= 21 && day <= 31) || (month ==  6 && day >= 01 && day <= 20))
+            zodiac_sign_name="Gemini";
+        else if ((month ==  06 && day >= 21 && day <= 30) || (month ==  7 && day >= 01 && day <= 22))
+            zodiac_sign_name="Cancer";
+        else if ((month ==  07 && day >= 23 && day <= 31) || (month ==  8 && day >= 01 && day <= 22))
+            zodiac_sign_name="Leo";
+        else if ((month ==  8 && day >= 23 && day <= 31) || (month ==  9 && day >= 01 && day <= 22))
+            zodiac_sign_name="Virgo";
+        else if ((month ==  9 && day >= 23 && day <= 30) || (month == 10 && day >= 01 && day <= 22))
+            zodiac_sign_name="Libra";
+        else if ((month == 10 && day >= 23 && day <= 31) || (month == 11 && day >= 01 && day <= 21))
+            zodiac_sign_name="Scorpio";
+        else if ((month == 11 && day >= 22 && day <= 30) || (month == 12 && day >= 01 && day <= 21))
+            zodiac_sign_name="Sagittarius";
+
+        //Get Chinese Zodiac Sign
+        String[] chinese_zodiac_name={"Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Sheep"};
+        int count=0;
+        for(int i=1920;i<=year;i++)
+        {
+            if(year==i)
+            {
+                chi_zodiac_sign_name=chinese_zodiac_name[count];
+                if(count==0) {
+                    chinese_img=R.mipmap.monkey;
+                }
+
+            }
+            count++;
+            if(count>11)
+                count=0;
+        }
+
+        //update Firebase data storage
+        databaseReference.child(UniqueID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                dataSnapshot.getRef().child("Name").setValue(updated_email);
+                dataSnapshot.getRef().child("date_of_birth").setValue(birthdate);
+                dataSnapshot.getRef().child("zodiac_sign").setValue(zodiac_sign_name);
+                dataSnapshot.getRef().child("chinese_zodiac_sign").setValue(chi_zodiac_sign_name);
+
+                zodiacName.setText(zodiac_sign_name);
+                chineseZodName.setText(chi_zodiac_sign_name);
+                pd.dismiss();
+                Toast.makeText(getActivity(), "Data Updated", Toast.LENGTH_SHORT).show();
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("User", databaseError.getMessage());
+                pd.dismiss();
+            }
+        });
+          pd.dismiss();
+    }
+
+    private void setDataTime() {
+        //   fromDateEtxt.setOnClickListener(this);
+        final Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                dateOfBirth.setGravity(Gravity.LEFT);
+                dateFormatter.format(newDate.getTime());
+                int month = Calendar.MONTH;
+                newCalendar.get(month);
+
+                dateOfBirth.setText( dateFormatter.format(newDate.getTime()));
+
+
+
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
     }
 
@@ -254,29 +364,46 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
 
         if(view==button_upload)
         {
-            upload();
+            //upload();
             button_gallary.setVisibility(view.VISIBLE);
 
         }
         if(view==editName)
         {
+            name.setEnabled(true);
+
 
         }
         if(view==calender)
         {
+            name.setEnabled(false);
+            dateOfBirth.setEnabled(true);
+            zodiacName.setEnabled(false);
+            chineseZodName.setEnabled(false);
+            fromDatePickerDialog.show();
 
         }
         if(view==editDate)
         {
+            name.setEnabled(false);
+            dateOfBirth.setEnabled(true);
+            zodiacName.setEnabled(false);
+            chineseZodName.setEnabled(false);
+            fromDatePickerDialog.show();
 
         }
         if(view==zodSign)
         {
 
+
         }
 
         if(view==editZodSign)
         {
+            name.setEnabled(true);
+            dateOfBirth.setEnabled(false);
+            zodiacName.setEnabled(true);
+            chineseZodName.setEnabled(false);
 
         }
         if(view==chineseSign)
@@ -285,10 +412,43 @@ public class Friend_Edit_DeleteFragment extends Fragment implements View.OnClick
         }
         if(view==editChineseSign)
         {
+            name.setEnabled(true);
+            dateOfBirth.setEnabled(false);
+            zodiacName.setEnabled(false);
+            chineseZodName.setEnabled(true);
 
         }
         if(view==save)
         {
+
+            name.setEnabled(false);
+            dateOfBirth.setEnabled(false);
+            zodiacName.setEnabled(false);
+            chineseZodName.setEnabled(false);
+            String fname=name.getText().toString();
+            String birthdate=dateOfBirth.getText().toString();
+
+            if (fname.isEmpty()) {
+
+
+                name.setError("Please Provide your Friend Name");
+            }
+                 if(birthdate.isEmpty())
+                {
+
+                    dateOfBirth.setError("Please Select Friend's Birthdate");
+                }
+
+             else {
+
+
+                String uniqueID = fname.concat(birthdate);
+                name.setError(null);
+                dateOfBirth.setError(null);
+                upload(uniqueID);
+                updateData_loadTofirebase(uniqueID);
+
+            }
 
         }
         if(view==delete)
